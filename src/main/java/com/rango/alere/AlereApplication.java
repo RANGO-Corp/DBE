bfc07bfb4dc9cb2a602bc57ff019a61921d2736c
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-11-05 00:00:38 -0300

    commit inicial

---
+++ src/main/java/com/rango/alere/AlereApplication.java
@@ -0,0 +1,13 @@
+package com.rango.alere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rango/alere/AlereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rango.alere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
