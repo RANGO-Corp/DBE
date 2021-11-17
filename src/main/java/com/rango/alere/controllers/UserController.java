2a5b6921db5eb4cb0fd5752c54e39246d4eac782
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-11-17 20:18:47 -0300

    refactor pages

---
+++ src/main/java/com/rango/alere/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.rango.alere.controllers;
+
+import com.rango.alere.entities.Usuario;
+import com.rango.alere.facades.UserFacade;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserFacade userFacade;
+
+    @ModelAttribute("currentUser")
+    private Usuario getCurrentUser() {
+        return userFacade.getCurrentUser();
+    }
+
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
+
+}
